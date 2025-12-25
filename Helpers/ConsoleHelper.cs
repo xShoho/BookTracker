58ad08f8c801a5ffe68108b77c58ecbb5c701b21
Kamil Starosta <ks@g3w.eu>
2025-12-25 16:58:22 +0100

    Implemented Main menu

--- Helpers/ConsoleHelper.cs
+++ Helpers/ConsoleHelper.cs
@@ -2,8 +2,96 @@ namespace BookTracker.Helpers;
 
 class ConsoleHelper
 {
-    public static void pritnText(string text)
+    public static int DisplayMainMenu(string? message = "Welcome To BookTracker")
     {
-        Console.WriteLine(text);
+        var options = new List<string>();
+        options.Add("Add New Book");
+        options.Add("View All Books");
+        options.Add("View Books By Status");
+        options.Add("View Books By Genre");
+        options.Add("Mark Book As Read");
+        options.Add("Remove Book");
+        options.Add("View Statistics");
+        options.Add("Exit");
+
+        int currentOption = 0;
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            Console.ResetColor();
+            if (message != string.Empty)
+                PrintCentered(message);
+
+            foreach (var (option, index) in options.Select((value, idx) => (value, idx)))
+            {
+                if (currentOption == index)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"  {option}");
+                }
+                else
+                {
+                    Console.ResetColor();
+                    Console.WriteLine($"   {option}");
+                }
+            }
+
+            var key = Console.ReadKey(true);
+
+            switch (key.Key)
+            {
+                case ConsoleKey.DownArrow:
+                    if (currentOption < options.Count - 1)
+                        currentOption++;
+
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (currentOption > 0)
+                        currentOption--;
+
+                    break;
+                case ConsoleKey.J:
+                    if (currentOption < options.Count - 1)
+                        currentOption++;
+
+                    break;
+                case ConsoleKey.K:
+                    if (currentOption > 0)
+                        currentOption--;
+
+                    break;
+                case ConsoleKey.Enter:
+                    running = false;
+
+                    break;
+                case ConsoleKey.L:
+                    running = false;
+
+                    break;
+            }
+        }
+
+        return currentOption;
+    }
+
+    private static void PrintCentered(string? text, int width = 50)
+    {
+        int textWithPadding = text.Length + 2;
+        int remainingSpace = width - textWithPadding;
+
+        if (remainingSpace <= 0)
+        {
+            Console.WriteLine($" {text} \n");
+            return;
+        }
+
+        int equalsPerSide = remainingSpace / 2;
+        string left = new string('=', equalsPerSide);
+        string right = new string('=', remainingSpace - equalsPerSide);
+
+        Console.WriteLine($"{left} {text} {right}\n");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿namespace BookTracker;
-
-using BookTracker.Helpers;
+﻿using BookTracker.Helpers;
 using BookTracker.Services;
+
+var bookService = new BookService();
+bool running = true;
+string message = "Welcome To BookTracker";
+
+// 0 - Add New Book
+// 1 - View All Books
+// 2 - View Books By Status
+// 3 - View Books By Genre
+// 4 - Mark Book As Read
+// 5 - Remove Book
+// 6 - View Statistics
+// 7 - Exit
+
+while (running)
+{
+    int userMenuAction = ConsoleHelper.DisplayMainMenu(message);
+
+    switch (userMenuAction)
+    {
+        case 7:
+            running = false;
+
+            break;
+        default:
+            message = "Not Valid Option";
+            break;
+    }
+}
